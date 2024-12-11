b604d142cf99dbd56f6c13ee2fde9e2bce7bcdd8
Rohan Buch <buchrohand2@gmail.com>
2024-12-10 18:07:47 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,19 @@ decode_results results;
 #define NOTE_A4  440
 #define NOTE_B4  494
 #define NOTE_C5  523
+#define NOTE_Ab2 104
+#define NOTE_B3 263
+#define NOTE_Bb3 233
+#define NOTE_Fs3 185
+#define NOTE_Eb3 156
+#define NOTE_Cs2 69
+#define NOTE_Eb2 78
+#define NOTE_B2 124
+#define NOTE_Eb5 622
+//note eb2
+#define NOTE_E5 659
+#define NOTE_Fs2 93
+#define NOTE_Fs5 740
 #define NOTE_REST 0
 
 #define MAX_DOTS 10
@@ -28,18 +41,29 @@ decode_results results;
 #define IR_MOVE_RIGHT 0x2318F92C
 
 const uint16_t song[] PROGMEM = {
-    NOTE_C4,
-    NOTE_D4, 
-    NOTE_E4, 
-    NOTE_F4, 
-    NOTE_G4,
-    NOTE_A4,
-    NOTE_B4,
-    NOTE_C5,
+    NOTE_Ab2,
+    NOTE_B3, 
+    NOTE_Bb3, 
+    NOTE_Fs3, 
+    NOTE_Eb3,
+    NOTE_Cs2,
+    NOTE_Eb2,
+    NOTE_B2,
+    NOTE_Eb5,
+    NOTE_Eb2,
+    NOTE_E5,
+    NOTE_Fs2,
+    NOTE_Fs5,
     NOTE_REST
 };
 
 #define SONG_LENGTH (sizeof(song) / sizeof(song[0])) // Number of elements
+uint16_t currentNoteIndex = 0;
+uint16_t noteDuration = 500; //duration of each note in ms
+uint16_t notePause = 50;
+uint16_t noteTimer = 0;
+uint8_t playingSong = 1;
+uint8_t playFullSong = 0; 
 
 void playNote(uint16_t frequency) {
     if (frequency == NOTE_REST) {
@@ -54,7 +78,7 @@ void playNote(uint16_t frequency) {
     OCR1A = ocrValue;
     
 
-    // Enable PWM on PORTD6
+    // Enable PWM on PIN PB1
     PORTD = SetBit(PORTB, PB1, 1);
     TCCR1B = (TCCR1B & 0xF8) | 0x02; // Set prescaler to 8
 }
@@ -84,7 +108,7 @@ void playSong() {
 // Exercise 1: 3 tasks
 // Exercise 2: 5 tasks
 // Exercise 3: 7 tasks
-#define NUM_TASKS 5
+#define NUM_TASKS 6
 
 
 //Task struct for concurrent synchSMs implmentations
@@ -132,6 +156,7 @@ const unsigned long SCREEN_PERIOD = 1;
 const unsigned long GREENDOT_PERIOD =  5;
 const unsigned long BUTTON_PERIOD = 100;
 const unsigned long IR_PERIOD = 500;
+const unsigned long SONG_PERIOD = 50;
 
 task tasks[NUM_TASKS]; // declared task array with NUM_TASKS amount of tasks
 Dot dots[MAX_DOTS]; //array of dots
@@ -145,13 +170,16 @@ enum Joystick_States {JOYSTICK_IDLE, JOYSTICK_READ };
 enum Screen_States {SCREEN_UPDATE};
 enum DotTask_States { DOTS_MOVE };
 enum ButtonHandler_States { OFF, PRESS, ON, PRESS2};
-enum Game_States {OFF1, HOME_SCREEN, GAME1, GAME1END, GAME2, END};
+enum Game_States {OFF1, HOME_SCREEN, GAME1, GAME1END, GAME2, GAME2END, GAME3, END};
 enum IR_States { IR_IDLE, IR_DECODE};
+enum SongStates { SONG_IDLE, SONG_PLAY_NOTE, SONG_PAUSE};
+
 
 unsigned char systemOn = 0;
 unsigned char gameOn = 0;
 unsigned char gameWon = 0;
 unsigned int numFails = 0;
+unsigned char resetting = 0;
 void TimerISR() {
     
     //TODO: sample inputs here
@@ -234,27 +262,95 @@ void DrawRectangle(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint
     
 }
 
-const uint16_t sprite[5][5] = {
-    {0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0}, //greenborder
-    {0x07E0, 0xF800, 0xF800, 0xF800, 0x07E0}, //green edges and red center
-    {0x07E0, 0xF800, 0xF800, 0xF800, 0x07E0},
-    {0x07E0, 0xF800, 0xF800, 0xF800, 0x07E0},
-    {0x07E0, 0x07E0, 0x07E0, 0x07E0, 0x07E0}  //greenborder
-};
+void fill_screen(uint8_t start_col, uint8_t end_col, uint8_t start_row, uint8_t end_row, uint8_t red, uint8_t green, uint8_t blue)
+{
+    Send_Command(0x2A);
+    Send_Data(0);
+    Send_Data(start_col);
+    Send_Data(0);
+    Send_Data(end_col);
 
-void DrawSprite(uint16_t x, uint16_t y, uint16_t width, uint16_t height, const uint16_t sprite[5][5], uint16_t color) {
-    DrawRectangle(x, y, x + width - 1, y + height - 1, color); 
-    Send_Command(0x2C); 
+    Send_Command(0x2B);
+    Send_Data(0);
+    Send_Data(start_row);
+    Send_Data(0);
+    Send_Data(end_row);
 
-    // Loop through the sprite data
-    for (uint16_t row = 0; row < height; row++) {
-        for (uint16_t col = 0; col < width; col++) {
-            uint16_t color = sprite[row][col];
-            Send_Data((color >> 8) & 0xFF); //high
-            Send_Data(color & 0xFF);       //low
+    Send_Command(0x2C);
+    for (uint16_t row = start_row; row <= end_row; ++row) {
+        for (uint16_t col = start_col; col <= end_col; ++col) {
+            Send_Data(red);   // Red value
+            Send_Data(green); // Green value
+            Send_Data(blue);  // Blue value
         }
     }
 }
+
+const uint8_t sprite[8] = {
+    0b00111100,   
+    0b01000010,   
+    0b10100101, 
+    0b10000001, 
+    0b10100101, 
+    0b10011001, 
+    0b01000010, 
+    0b00111100   
+};
+
+const uint8_t digits[10][7] = {
+    {0b01110, 0b10001, 0b10011, 0b10101, 0b11001, 0b10001, 0b01110}, // 0
+    {0b00100, 0b01100, 0b00100, 0b00100, 0b00100, 0b00100, 0b01110}, // 1
+    {0b01110, 0b10001, 0b00001, 0b00110, 0b01000, 0b10000, 0b11111}, // 2
+    {0b01110, 0b10001, 0b00001, 0b00110, 0b00001, 0b10001, 0b01110}, // 3
+    {0b00010, 0b00110, 0b01010, 0b10010, 0b11111, 0b00010, 0b00010}, // 4
+    {0b11111, 0b10000, 0b11110, 0b00001, 0b00001, 0b10001, 0b01110}, // 5
+    {0b01110, 0b10001, 0b10000, 0b11110, 0b10001, 0b10001, 0b01110}, // 6
+    {0b11111, 0b00001, 0b00010, 0b00100, 0b01000, 0b01000, 0b01000}, // 7
+    {0b01110, 0b10001, 0b10001, 0b01110, 0b10001, 0b10001, 0b01110}, // 8
+    {0b01110, 0b10001, 0b10001, 0b01111, 0b00001, 0b10001, 0b01110}  // 9
+};
+
+void drawSprite(uint8_t x, uint8_t y, const uint8_t *sprite, uint8_t width, uint8_t height, uint8_t red, uint8_t green, uint8_t blue) {
+    for (uint8_t row = 0; row < height; row++) {
+        for (uint8_t col = 0; col < width; col++) {
+            // Check if the bit is set
+            if (sprite[row] & (1 << (width - 1 - col))) {
+                // Draw a single pixel
+                fill_screen(x + col, x + col, y + row, y + row, red, green, blue);
+            }
+        }
+    }
+}
+
+void displayNumFails(uint8_t x, uint8_t y, uint16_t numFails, uint8_t red, uint8_t green, uint8_t blue) {
+    uint8_t digitWidth = 5;  //width of each digit
+    uint8_t digitHeight = 7; //height of each digit
+    uint8_t spacing = 2;     //space between digits
+    uint8_t positionX = x;   //starting position for the first digit
+    
+    if (numFails == 0) {
+        //special case: display '0' if numFails is 0
+        drawSprite(positionX, y, digits[0], digitWidth, digitHeight, red, green, blue);
+        return;
+    }
+
+    //extract digits from numFails (right to left)
+    uint16_t temp = numFails;
+    uint8_t digitsToDraw[5]; //assuming max 5 digits
+    uint8_t digitCount = 0;
+
+    while (temp > 0) {
+        digitsToDraw[digitCount++] = temp % 10;
+        temp /= 10;
+    }
+
+    //draw digits (left to right)
+    for (int i = digitCount - 1; i >= 0; i--) {
+        drawSprite(positionX, y, digits[digitsToDraw[i]], digitWidth, digitHeight, red, green, blue);
+        positionX += digitWidth + spacing; //move to the next digit position
+    }
+}
+
 
 void black_screen()
 {
@@ -279,29 +375,7 @@ void black_screen()
     }
 }
 
-void fill_screen(uint8_t start_col, uint8_t end_col, uint8_t start_row, uint8_t end_row, uint8_t red, uint8_t green, uint8_t blue)
-{
-    Send_Command(0x2A);
-    Send_Data(0);
-    Send_Data(start_col);
-    Send_Data(0);
-    Send_Data(end_col);
 
-    Send_Command(0x2B);
-    Send_Data(0);
-    Send_Data(start_row);
-    Send_Data(0);
-    Send_Data(end_row);
-
-    Send_Command(0x2C);
-    for (uint16_t row = start_row; row <= end_row; ++row) {
-        for (uint16_t col = start_col; col <= end_col; ++col) {
-            Send_Data(red);   // Red value
-            Send_Data(green); // Green value
-            Send_Data(blue);  // Blue value
-        }
-    }
-}
 //128x128
 void create_sprite(uint8_t cx, uint8_t cy, uint8_t r, uint8_t blue, uint8_t green, uint8_t red)
 {
@@ -354,6 +428,105 @@ void create_sprite(uint8_t cx, uint8_t cy, uint8_t r, uint8_t blue, uint8_t gree
             }
         }
     }
+}
+
+void create_coin(uint8_t xn, uint8_t yn, uint8_t r, uint8_t blue, uint8_t green, uint8_t red)
+{
+    
+    // Define the bounding box for the circle
+    uint8_t x_start = (xn > r) ? (xn - r) : 0;
+    uint8_t x_end = (xn + r < 128) ? (xn + r) : 127;
+    uint8_t y_start = (yn > r) ? (yn - r) : 0;
+    uint8_t y_end = (yn + r < 128) ? (yn + r) : 127;
+
+    // Set the column address (x range)
+    Send_Command(0x2A); 
+    Send_Data(0);      
+    Send_Data(x_start); 
+    Send_Data(0);      
+    Send_Data(x_end);   
+
+    // Set the row address (y range)
+    Send_Command(0x2B); 
+    Send_Data(0);       
+    Send_Data(y_start); 
+    Send_Data(0);       
+    Send_Data(y_end);   
+
+    // Begin writing pixel data
+    Send_Command(0x2C); // Memory write
+
+    for (uint8_t y = y_start; y <= y_end; ++y) //square/rectangular dimensions
+    {
+        for (uint8_t x = x_start; x <= x_end; ++x)
+        {
+            // Check if the pixel is within the circle
+            uint16_t dx = x - xn;
+            uint16_t dy = y - yn;
+            uint16_t distance_squared = dx * dx + dy * dy;
+
+            if (distance_squared <= r * r)
+            {
+                // Inside the circle: Set a color
+                Send_Data(blue); //blue
+                Send_Data(green); //green
+                Send_Data(red); // red
+            }
+            else
+            {
+                //outside the circle, but within rectangular/square dimensions
+                Send_Data(0);
+                Send_Data(0);
+                Send_Data(0);
+            }
+        }
+    }
+}
+
+//--- Tasks Below ---
+uint16_t elapsedTime = 0;
+int Song_Tick(int state)
+{
+    
+    switch(state)
+    {
+        case SONG_IDLE:
+            
+                if (currentNoteIndex < SONG_LENGTH && gameWon)
+                {
+                    uint16_t note = pgm_read_word(&song[currentNoteIndex]);
+                    playNote(note);
+                    
+                    state = SONG_PLAY_NOTE;
+                    
+                }
+                else
+                {
+                    currentNoteIndex = 0;
+                }
+            
+            break;
+
+        case SONG_PLAY_NOTE:
+            if (elapsedTime >= 500)
+            {
+                elapsedTime = 0;
+                currentNoteIndex++;
+                state = SONG_IDLE;
+                
+            }
+            elapsedTime += SONG_PERIOD;
+            break;
+        
+        
+        
+        default:
+            state = SONG_IDLE;
+            break;
+
+    }
+    
+    return state;
 }
 
 volatile uint8_t cx = 6; //moveable character
@@ -418,9 +591,14 @@ int IR_Tick(int state)
                 irCommand = results.value;
                 irNewCommand = true;
                 //IRresume();
+                //serial_println(results.value);
                 if (results.value == 16718055) // '2'
                 {
                     systemOn = !systemOn;
+                }
+                else if (results.value == 16743045) // '3'
+                {
+                    resetting = !resetting;
                 }
             }
             state = IR_DECODE;
@@ -608,6 +786,7 @@ int ButtonHandler_Tick(int state)
             {
                 state = ON;
                 systemOn = 1;
+                
             }
             break;
         case ON:
@@ -621,7 +800,7 @@ int ButtonHandler_Tick(int state)
             {
                 state = OFF;
                 systemOn = 0;
-                //Send_Command(0x28); //turn off display
+                Send_Command(0x11); //turn off display
             }
             break;
         default:
@@ -631,6 +810,8 @@ int ButtonHandler_Tick(int state)
     return state;
 }
 
+unsigned char coinReceived = 0;
+unsigned char coinVisible = 1;
 int Game_Tick(int state)
 {
     switch(state)
@@ -663,6 +844,7 @@ int Game_Tick(int state)
                 add_wall(100, 114, 15, 116); //red are the only walls.
                 numDots = 2;
                 
+                
             }
             
             //add title
@@ -693,7 +875,7 @@ int Game_Tick(int state)
                     numFails++;
                 }
 
-                if (GetBit(PINC, PC2) == 0)
+                if (GetBit(PINC, PC2) == 0 || resetting)
                 {
                     gameOn = 0;
                     state = HOME_SCREEN;
@@ -706,13 +888,13 @@ int Game_Tick(int state)
                 {
                     state = GAME1END;
                     //gameOn = 1;
-                    
+                    //playFullSong = 1;
 
                 }
             if (!systemOn)
             {
                 state = OFF1;
-                Send_Command(0x10); //turn off
+                
                 
             }
             break;
@@ -770,7 +952,7 @@ int Game_Tick(int state)
                     numFails++;
                 }
 
-                if (GetBit(PINC, PC2) == 0)
+                if (GetBit(PINC, PC2) == 0 || resetting)
                 {
                     gameOn = 0;
                     state = GAME1END;
@@ -779,11 +961,12 @@ int Game_Tick(int state)
             }
             if (cx <= 130 && cy <= 20)
                 {
-                    state = END;
-                    num_walls = 0;
-                    fill_screen(0, 130, 0, 130, 0, 0, 0);
+                    state = GAME2END;
+                    //gameWon = 1;
                     
-
+                    fill_screen(0, 130, 0, 130, 0, 0, 0);
+                    //drawSprite(64, 64, sprite, 8, 8, 255, 0, 0);
+                    //displayNumFails(32, 32, numFails, 255, 0, 0);
 
                 }
             if (!systemOn)
@@ -795,6 +978,99 @@ int Game_Tick(int state)
             
             break;
 
+        case GAME2END:
+            if (systemOn)
+            {
+                state = GAME3;
+                num_walls = 0;
+                cx = 64;
+                cy = 125;
+                gameOn = 1;
+                init_dot(0, 64, 77, -2, 0, 5, 0, 255, 0, 0, 130, 0, 115);
+                init_dot(1, 64, 92, 2, 0, 5, 0, 255, 0, 0, 130, 0, 115);
+                init_dot(2, 64, 62, 2, 0, 5, 0, 255, 0, 0, 130, 0, 115); //vert doesnt matter if dy is 0 anyway
+                init_dot(3, 64, 47, -2, 0, 5, 0, 255, 0, 0, 130, 0, 115); //more dots = more speed
+                fill_screen(0, 49, 100, 115, 0, 0, 250); //red
+                fill_screen(80, 130, 100, 115, 0, 0, 250); //red
+                fill_screen(0, 49, 15, 30, 0, 0, 250); //red
+                fill_screen(80, 130, 15, 30, 0, 0, 250); //red
+                fill_screen(0, 130, 0, 15, 0, 0, 0); //black
+                fill_screen(49, 80, 15, 30, 0, 0, 0); //black
+                fill_screen(0, 130, 30, 100, 0, 0, 0); //black
+                fill_screen(49, 80, 100, 115, 0, 0, 0); //black
+                fill_screen(0, 130, 115, 130, 0, 0, 0);
+                add_wall(0, 49, 100, 115);
+                add_wall(80, 130, 100, 115);
+                add_wall(0, 49, 15, 30); //red are the only walls.
+                add_wall(80, 130, 15, 30);
+                
+                numDots = 4;
+            }
+            break;
+
+        case GAME3:
+            create_sprite(cx, cy, radius, 0, 0, 255); //moveable sprite
+            
+
+            if (coinVisible)
+            {
+                create_coin(64, 64, radius, 0, 255, 255);
+            }
+            
+            for (uint8_t i = 0; i < numDots; i++) //create each evil dot sprite
+            {
+                create_sprite(dots[i].x - dots[i].dx, dots[i].y - dots[i].dy, 
+                            dots[i].radius, 0, 0, 0);
+
+                create_sprite(dots[i].x, dots[i].y, dots[i].radius, dots[i].red,
+                dots[i].green, dots[i].blue); 
+
+                uint16_t dx = cx - dots[i].x;
+                uint16_t dy = cy - dots[i].y;
+                uint16_t distance_squared = dx * dx + dy * dy;
+
+                if (distance_squared <= ((dots[i].radius+5) * (dots[i].radius+5)))
+                {
+                    gameOn = 0;
+                    state = GAME2END;
+                    numFails++;
+                    coinReceived = 0;
+                    coinVisible = 1;
+                }
+
+                if (GetBit(PINC, PC2) == 0 || resetting)
+                {
+                    gameOn = 0;
+                    state = GAME2END;
+                }
+                
+                
+            }
+            if ((cx >= 60 && cx <= 68) && (cy >= 60 && cy <= 68))
+                {
+                    coinVisible = 0;
+                    coinReceived = 1;
+                    fill_screen(64 - radius, 64 + radius, 64 - radius, 64 + radius, 0, 0, 0);
+                }
+            
+            if (cx <= 130 && cy <= 20 && coinReceived)
+                {
+                    state = END;
+                    gameWon = 1;
+                    
+                    fill_screen(0, 130, 0, 130, 0, 255, 0);
+                    drawSprite(64, 64, sprite, 8, 8, 255, 0, 0);
+                    displayNumFails(32, 32, numFails, 255, 0, 0);
+
+                }
+            if (!systemOn)
+            {
+                state = OFF1;
+                Send_Command(0x10);
+                
+            }
+            
+            break;
         case END:
             break;
         default:
@@ -871,6 +1147,11 @@ int main(void) {
     tasks[4].period = BUTTON_PERIOD;
     tasks[4].elapsedTime = 0;
     tasks[4].TickFct = &ButtonHandler_Tick;
+
+    tasks[5].state = SONG_IDLE;
+    tasks[5].period = 100;
+    tasks[5].elapsedTime = 0;
+    tasks[5].TickFct = &Song_Tick;
 
     TimerSet(GCD_PERIOD);
     TimerOn();
